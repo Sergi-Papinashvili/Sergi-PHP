0de0742a3d367709491ca5200364643bd47f90a6
Sergi-Papinashvili <38999999+Sergi-Papinashvili@users.noreply.github.com>
2019-04-05 11:35:37 +0400

    Update index.php

--- L101/index.php
+++ L101/index.php
@@ -1,31 +1,48 @@
-﻿<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<pre>
 <?php
-$num = 0;
-$num2 = 0;
-$cars = array();
-for($x = 0; $x<12; $x++) {
-    $cars[$x] = rand(10,100);
-}
-for($x = 0; $x<12; $x++) {
-if($cars[$x] > 88    ){
-$num = $num + 1;
- }
- else{
-$num2 = $num2 + 1;
- }
-}
-echo "<span>x ზე მეტია </span>".$num ."<br>" ; 
-echo "<span>x ზე ნაკლებია </span>".$num2 ."<br>" ; 
+if(isset($_GET["name"]) || isset($_GET["surname"]) || isset($_GET["salary"]) || isset($_GET["position"]))
+$name= "name";
+$surname= "surname";
+$salary= "salary";
+$position= "position";
 ?>
-</pre>
+    
+<html>
+<body>
+<div class="form" style="width:500px;">
+<form action="<?php $PHP_SELF ?>" method ="GET">
+name:<br> <input type="text" name="name" style="width:200px;"/><br>
+lastname: <br> <input type="text" name="lastname" style="width:200px;"/><br>
+salary: <br><input type="text" name="salary"style="width:200px;" /><br>
+position: <br> <input type="text" name="position" style="width:200px;"/><br>
+<form action="submit. <?php?>" method ="GET"><br>
+<input type="submit"/><br>
+</form>
+</form>
+</div>
+
+<table  border="1px">
+<tr >
+<td style="width:300px;"><?php echo  'name:';?> </td>
+<td style="width:300px;"><?php echo $_GET["name"]; ?><br /></td>
+</tr>
+
+<tr>
+<td><?php echo  'lastname:';?></td>
+<td><?php echo $_GET["lastname"]; ?><br /></td>
+</tr>
+
+<tr>
+<td><?php echo  'salary:';?></td>
+<td><?php echo $_GET["salary"]; ?><br /></td>
+</tr>
+
+<tr>
+<td><?php echo 'position:';?></td>
+<td><?php echo $_GET["position"]; ?><br /></td>
+</tr>
+
+</table>
+
+
 </body>
 </html>
